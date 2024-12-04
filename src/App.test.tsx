17b3fb7cdeabd7ba51262605f6f84e8ff40bf23e
Quincy Kwende <quincykwende@gmail.com>
2024-12-04 11:10:54 +0100

    init(REC-001) --with react, typescript + material ui

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Recipes/i);
   expect(linkElement).toBeInTheDocument();
 });
+
