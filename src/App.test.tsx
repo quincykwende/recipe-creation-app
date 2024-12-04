34949aa70b5789dda8ffa6df413aeb4c3aad4e0b
Quincy Kwende <quincykwende@gmail.com>
2024-12-04 11:53:03 +0100

    feat(REC-002) added recipeName input field + state management

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import {fireEvent, render, screen} from '@testing-library/react'
+import App from './App'
 
 test('renders app title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Recipes/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/Recipes/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test("renders recipeName input and updates state", ()=> {
+  render(<App />)
+  const input = screen.getByLabelText(/Recipe Name/i)
+  expect(input).toBeInTheDocument()
+
+  fireEvent.change(input, {target: {value: 'test name'}})
+  expect(input).toHaveValue('test name')
+})
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import {Container, Typography} from '@mui/material'
-import './App.css';
+import React, {useState} from 'react'
+import {Container, Typography, TextField} from '@mui/material'
+import './App.css'
 
 function App() {
+  const [recipeName, setRecipeName] = useState<string>('')
+
   return (
       <Container>
         <Typography variant="h4" gutterBottom>
           Recipes
         </Typography>
+        <TextField
+            label="Recipe Name"
+            value={recipeName}
+            onChange={ (e) => setRecipeName(e.target.value)}
+        />
       </Container>
   );
 }
 
-export default App;
+export default App
